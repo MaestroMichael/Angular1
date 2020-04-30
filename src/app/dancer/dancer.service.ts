3e8c08e2092fb9153eec9faaffd37e280fad770f
MaestroMichael <zapart.michal7@gmail.com>
2020-04-30 13:52:11 +0200

    Changes in code

--- src/app/dancer/dancer.service.ts
+++ src/app/dancer/dancer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CoffinDancers } from './dancer';
 import { Storage } from '@ionic/storage';
 import { table } from './dancerTemplate';
@@ -11,17 +11,10 @@ export class DancerService {
 
   private table: CoffinDancers[] = table;
 
-  public workersData: BehaviorSubject<CoffinDancers[]> = new BehaviorSubject<CoffinDancers[]>(this.table)
-
-  constructor(private storage: Storage) { }
-  public load(): void {
-
-    this.storage.get('workersData').then((data) => {
-
-      this.workersData.next(data);
-
-    });
-
+  public workersData: BehaviorSubject<CoffinDancers[]> = new BehaviorSubject<CoffinDancers[]>(this.table);
+  /*example how to handle BehabiorSubject is taken form documentation of CDK data-table  */
+  connect(): Observable<CoffinDancers[]> {
+    return this.workersData;
   }
-
+  disconnect() {}
 }
--- src/app/dancer/dancerTemplate.ts
+++ src/app/dancer/dancerTemplate.ts
@@ -1,24 +1,24 @@
 export const table = [
     {
-      img: './asset/coffin4.jpg',
+      img: './assets/coffin4.jpg',
       name: 'Benjamin Aidoo',
       possition: 'leader',
       company: 'Nana Otafrija Pallbearing & Waiting Services',
     },
     {
-      img: './asset/coffin1.jpg',
+      img: './assets/coffin1.jpg',
       name: 'dancer1',
       possition: 'dancer',
       company: 'Nana Otafrija Pallbearing & Waiting Services',
     },
     {
-      img: './asset/coffin2.jpg',
+      img: './assets/coffin2.jpg',
       name: 'dancer 2',
       possition: 'dancer',
       company: 'Nana Otafrija Pallbearing & Waiting Services',
     },
     {
-      img: './asset/coffin3.jpg',
+      img: './assets/coffin3.jpg',
       name: 'dancer 3',
       possition: 'dancer',
       company: 'Nana Otafrija Pallbearing & Waiting Services',
